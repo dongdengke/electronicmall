76e3bfa894bf71a4f07971113de18b33dbf99fc6
dong <dong@dong-Inspiron-N4010>
2015-01-26 15:48:02 +0800

    finish cart

---
+++ src/cn/edu/bjtu/elctronicmall/view/OrderView.java
@@ -0,0 +1,25 @@
+package cn.edu.bjtu.elctronicmall.view;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+public class OrderView extends BaseView {
+
+	public OrderView(Context context, Bundle bundle) {
+		super(context, bundle);
+	}
+
+	@Override
+	public View getView(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
